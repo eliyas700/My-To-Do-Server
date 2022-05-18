0985be62af582beae05e54c5e7bba61603177ebb
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-18 23:47:34 +0600

    Add New Task

--- index.js
+++ index.js
@@ -19,20 +19,26 @@ async function run() {
   try {
     await client.connect();
     const tasksCollection = client.db("To-Do-APP").collection("tasks");
-
+    //Get all Tasks
     app.get("/tasks", async (req, res) => {
       const tasks = await tasksCollection.find().toArray();
       res.send(tasks);
+    });
+    //Add a Task
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+      console.log(newTask);
+      const result = await tasksCollection.insertOne(newTask);
+      res.send(result);
     });
   } finally {
   }
 }
 run().catch(console.dir);
-//Get all the tasks
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
