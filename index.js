ad2ae9dd69a2aa5c28b13e0bc08036b8ff674e01
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-18 23:23:07 +0600

    Send All the tASKs from db

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const cors = require("cors");
+
 //Middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.z9k4k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -18,9 +19,16 @@ async function run() {
   try {
     await client.connect();
     const tasksCollection = client.db("To-Do-APP").collection("tasks");
+
+    app.get("/tasks", async (req, res) => {
+      const tasks = await tasksCollection.find().toArray();
+      res.send(tasks);
+    });
   } finally {
   }
 }
+run().catch(console.dir);
+//Get all the tasks
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
